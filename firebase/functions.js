04aeb89846623ed7871fe51811c064babcb8c6a1
Guss <wrightgeospatial@gmail.com>
2020-01-14 16:31:56 -0500

    added click map to add marker functionality

--- firebase/functions.js
+++ firebase/functions.js
@@ -12,6 +12,8 @@ var radiusKM = 0;
 var searchRadius;
 var resultsList = [];
 var searchCenter;
+var lng;
+var lat;
 
 //GLOBALS
 
@@ -48,6 +50,84 @@ var searchCenter;
 // Search Around Geocoded Entry
 // $('#geocode-entry').change(findPlace);
 
+
+// Click Map to add Listing
+function addListing0(){
+
+    alert('Click on Map to add new Listing...');
+    
+    //Change Cursor
+    map.on('mousemove', (e) => {
+        map.getCanvas().style.cursor = 'crosshair';
+    })
+
+    // Click Listener
+    map.on('click', function (e) {
+        //Change Cursor
+        map.getCanvas().style.cursor = 'grab';
+
+        // Get Coords
+        lng = e.lngLat.lng;
+        lat = e.lngLat.lat;
+
+        //Show data form
+        document.getElementById("clickForm").style.display = "block";
+        
+        // create radius around user Location
+        //  L.circle(e.latlng, 500000).addTo(map);
+    });  
+}
+
+function runClickForm(){
+    // alert('Running...')
+    //  Declare Global Variables
+    var Name =  'No Data';
+    var Category  = 'No Data';
+    var Rating = 'No Data';
+    var Image_URL = 'No Data';
+    var Position = 'No Data';
+    
+
+
+    // GET Form Data
+
+    Name = document.getElementById("posted_by");
+    localStorage.setItem("0", Name.value);
+
+    Rating = document.getElementById("rating");
+    localStorage.setItem("1", Rating.value);
+
+    Category = document.getElementById("category");
+    localStorage.setItem("2", Category.value);
+
+    Image_URL = document.getElementById("img_url");
+    localStorage.setItem("3", Image_URL.value);
+
+    Position = document.getElementById("position");
+    localStorage.setItem("4", Position.value);
+
+    var entry0 = localStorage.getItem("0");
+    var entry1 = localStorage.getItem("1");
+    var entry2 = localStorage.getItem("2");
+    var entry3 = localStorage.getItem("3");
+    var entry4 = localStorage.getItem("4");
+
+    // GET Form Data END
+
+        // fbRef = Firebase Reference
+        fbRef.child('data').push()
+            .set({
+                Position: entry4, Rating: entry1,
+                Category: entry2, Image: entry3,
+                Posted: entry0, Latitude: lat,
+                Longitude: lng
+
+            });
+        
+        // alert('Pushed Success...')
+
+        location.reload();
+}
 
 // Fill GeoJson from Firebase Database Retrieve Data
 function populateMap() {
